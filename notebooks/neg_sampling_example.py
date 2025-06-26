34ccafff3a09bd6842f790c625d3c5bb954af875
Alexander Ledovsky <alexander.ledovsky@gmail.com>
2025-06-26 22:43:39 +0300

    added some pytorch

--- notebooks/neg_sampling_example.py
+++ notebooks/neg_sampling_example.py
@@ -2,7 +2,7 @@ import torch
 
 # 1) Create a batch of size B=2, emb dim D=4
 user_emb = torch.tensor([[1., 2., 3., 4.],
-                         [1., 2., 3., 4.]])   # shape (2,4)
+                         [11., 22., 33., 44.]])   # shape (2,4)
 pos_item_emb = user_emb.clone()           # same shape (2,4)
 
 B, D = user_emb.shape
