e5f95b46813a6f94f30edcac8344a4cea80e9f55
Alexander Ledovsky <alexander.ledovsky@gmail.com>
2025-07-02 20:06:51 +0300

    upd sync.sh

--- sync.sh
+++ sync.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 
 REMOTE_HOST=avi-ix-devbox02
-REMOTE_PATH=~/"$(basename "$(pwd)")"
+REMOTE_PATH="$(basename "$(pwd)")"
 
 # Sync git working tree
-rsync -avz --delete --exclude '.git/' ./ "${REMOTE_HOST}:${REMOTE_PATH}/"
+# rsync -avz --delete --exclude '.git/' ./ "${REMOTE_HOST}:${REMOTE_PATH}/"
+git ls-files -z | rsync --files-from=- --from0 -avz --delete ./ "${REMOTE_HOST}:~/${REMOTE_PATH}/"
+
+rsync -avz --delete ./.git/ "${REMOTE_HOST}:${REMOTE_PATH}/.git/"
 
 # Sync data directory
 rsync -avz --delete ./data/ "${REMOTE_HOST}:${REMOTE_PATH}/data/"
